b92b9fe3f5ce0cf3fd70c20e5420003907723f64
Bastiancito <bastian.gamboa@ug.uchile.cl>
2024-05-16 12:38:42 -0400

    updates

--- astro_queries/astro_object/astroframe.py
+++ astro_queries/astro_object/astroframe.py
@@ -1,22 +1,44 @@
 from ..queries import get_detections_and_force_photometry_by_oid
 from psql_connector import PgConnector
 import matplotlib.pyplot as plt
+import logging
 
 
 class AstroFrame:
 
     def __init__(self, oid: str = None, **kwargs) -> None:
 
+        self.oid = oid
         self.detections = None
         self.forced_photometry = None
         self.detections_filtered = None
         self.forced_photometry_filtered = None
+        self.psql_conn = PgConnector()
+
+    def _connect_to_database(self, **kwargs):
+        self.psql_conn.create_conn(**kwargs)
+
+    def _fetch_lightcurve(self):
+
+        if self.psql_conn.conn is not None:
+            logging.info("Fetching raw lightcurves")
+            astro_query_dd, astro_query_ff = get_detections_and_force_photometry_by_oid(
+                oid=self.oid
+            )
+            df_dd = self.psql_conn.sqlio_query(astro_query_dd)
+            df_ff = self.psql_conn.sqlio_query(astro_query_ff)
+
+            self.detections = df_dd
+            self.forced_photometry = df_ff
+        else:
+            logging.warning(
+                "PSQL connector was not initializateed, try to create db connection with '_connect_to_database' method."
+            )
 
     def deduplicate(self):
-
         self.detections_filtered = None
         self.forced_photometry_filtered = None
 
-    def plot_curves(self):
-
-        pass
+    def plot_curves(self, **kwargs):
+        fig, ax = plt.subplots(1, 2, figsize=(10, 5))
+        plt.close()
--- tests/unit/test_astroframe.py
+++ tests/unit/test_astroframe.py
@@ -1,9 +1,14 @@
 from astro_queries.astro_object import AstroFrame
+from psql_connector import PgConnector
+from unittest.mock import patch
 
 
-def test_astroframe():
+@patch("astro_queries.astro_object.AstroFrame")
+def test_astroframe(_):
     model = AstroFrame()
     assert model.detections == None
     assert model.forced_photometry == None
     assert model.detections_filtered == None
     assert model.forced_photometry_filtered == None
+    assert isinstance(model, AstroFrame)
+    assert isinstance(model.psql_conn, PgConnector)
